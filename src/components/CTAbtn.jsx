c02dcf28f7eec67e1dbaa4a164d680d517763440
AbdulQadirJillani <abdulqadir786687786@gmail.com>
2024-09-17 13:38:51 +0500

    version2

--- src/components/CTAbtn.jsx
+++ src/components/CTAbtn.jsx
@@ -2,8 +2,8 @@ import { Button } from "./ui/button"
 
 const CTAbtn = () => {
   return (
-    <a href="mailto:mikiyasaddis73@gmail.com" target="_blank">
-      <Button size="lg" className="flex justify-center items-center gap-2 bg-[#426cf5] text-white hover:bg-[#4242f5] active:scale-95 group">
+    <a href="mailto:mikiyasaddis73@gmail.com" target="_blank" className="">
+      <Button size="lg" className="w-full flex justify-center items-center gap-2 bg-[#426cf5] text-white hover:bg-[#4242f5] active:scale-95 group">
           <span className="font-bold text-lg">Get a Quote</span>
           <i className="pi pi-chevron-right font-bold group-hover:translate-x-1"/>
       </Button>
